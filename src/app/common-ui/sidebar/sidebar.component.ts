407f5e6f3e84622a0502290c05d84de0d1c8ff52
YusupOzdoyev <yusup.ozdoyev@mail.ru>
2024-06-21 18:16:35 +0300

    add settings, add routs, add change me

--- src/app/common-ui/sidebar/sidebar.component.ts
+++ src/app/common-ui/sidebar/sidebar.component.ts
@@ -1,28 +1,36 @@
-import { Component, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { SvgIconComponent } from '../svg-icon/svg-icon.component';
 import { CommonModule } from '@angular/common';
 import { SubscriberCardComponent } from "./subscriber-card/subscriber-card.component";
 import { RouterModule } from '@angular/router';
 import { ProfileService } from '../../data/services/profile.service';
+import { firstValueFrom } from 'rxjs';
+import { ImgUrlPipe } from "../../helpers/pipes/img-url.pipe";
 
 @Component({
     selector: 'app-sidebar',
     standalone: true,
     templateUrl: './sidebar.component.html',
     styleUrl: './sidebar.component.scss',
-    imports: [RouterModule, CommonModule, SvgIconComponent, SubscriberCardComponent]
+    imports: [
+        RouterModule,
+        CommonModule,
+        SvgIconComponent,
+        SubscriberCardComponent,
+        ImgUrlPipe
+    ]
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
   profileService = inject(ProfileService);
-  subscribers$ = this.profileService.getSubscribersSortList()
+  subscribers$ = this.profileService.getSubscribersShortList()
 
-  // me = this.profileService.me
+  me = this.profileService.me;
 
   menuItems = [
     {
       label: 'Моя страница',
       icon: 'home',
-      link: 'profile'
+      link: 'profile/me'
     },
     {
       label: 'Чаты',
@@ -35,4 +43,9 @@ export class SidebarComponent {
       link: 'search'
     }
   ]
+
+  ngOnInit() {
+    firstValueFrom(this.profileService.getMe())
+  }
+
 }
--- src/app/data/services/profile.service.ts
+++ src/app/data/services/profile.service.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable, inject } from '@angular/core';
+import { Injectable, inject, signal } from '@angular/core';
 import { Profile } from '../interfaces/profile.interface';
 import { Pageble } from '../interfaces/pageble.interface';
-import { map } from 'rxjs';
+import { map, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +10,8 @@ import { map } from 'rxjs';
 export class ProfileService {
   http = inject(HttpClient);
   baseApiUrl = 'https://icherniakov.ru/yt-course/';
+
+  me = signal<Profile | null>(null)
 
   getTestAccounts() {
     return this.http.get<Profile[]>(`${this.baseApiUrl}account/test_accounts`);
@@ -20,14 +22,21 @@ export class ProfileService {
   }
 
   getMe() {
-    return this.http.get<Profile>(`${this.baseApiUrl}account/me`);
+    return this.http.get<Profile>(`${this.baseApiUrl}account/me`)
+      .pipe(
+        tap(res => this.me.set(res))
+      )   
   }
 
-  getSubscribersSortList() {
+  getSubscribersShortList(subsAmount = 3) {
     return this.http.get<Pageble<Profile>>(`${this.baseApiUrl}account/subscribers/?page=1&size=50`)
       .pipe(
-        map(res => res.items.slice(0, 3))
+        map(res => res.items.slice(0, subsAmount))
       )
+  }
+
+  patchProfile(profile: Partial<Profile>) {
+    return this.http.patch<Profile>(`${this.baseApiUrl}account/me`, profile)
   }
 }
 
